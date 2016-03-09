1aaec9145f0fd89bfc344825f912c506522f525c
alex <ltaspro@mail.ru>
2016-03-09 11:12:31 +0200

    fix date

--- wp-content/themes/theme_classic/js/script.js
+++ wp-content/themes/theme_classic/js/script.js
@@ -19,13 +19,6 @@
         $(function () {
             $('.menu-item-44 a').text(' ');
         });
-        $('.post.type-post p:has(img)').css(
-            {
-                'text-align': 'center',
-                'margin': '45px auto'
-            }
-        );
-        $('.social-likes').before("<span class='social-title'>Share this article...</span>");
 
     });
 })(jQuery);
--- wp-content/themes/theme_classic/single.php
+++ wp-content/themes/theme_classic/single.php
@@ -10,24 +10,25 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<?php get_template_part( 'template-parts/category', 'template' );?>
+		<?php get_template_part( 'template-parts/category', 'template' ); ?>
 		<main id="main" class="site-main" role="main">
 
-		<div class="container-fluid">
-			<?php
-			while ( have_posts() ) : the_post();
-
-				get_template_part( 'template-parts/content', get_post_format() );
-
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
+			<div class="container-fluid">
+				<?php
+				while ( have_posts() ) :
+				the_post(); ?>
+				<article>
+					<h2 class="title-centered"><?php the_title(); ?></h2>
+					<span class="date title-centered">Posted on <?php echo get_the_date( 'j M, Y' ); ?></span>
+					<?php the_content(); ?>
+				</article>
+				<?php if ( comments_open() || get_comments_number() ) :
+				comments_template();
 				endif;
 
-			endwhile; // End of the loop.
-			?>
-		</div>
+				endwhile; // End of the loop.
+				?>
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
